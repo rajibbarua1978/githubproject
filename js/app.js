9dc806253f434a36e6fb61d53a2e0f006c16d79d
Rajib <kbrajib123@gmail.com>
2024-05-28 13:07:19 +0600

    initial message

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready{function(){
+
+
+
+    // *Slick Slider (Banner Slider)*\\
+    $('.bannerSlider').slick({
+      arrows : false,
+      dots : true,
+      dotsClass : 'bannerDots container',
+      
+    });
+
+    $('.bannerSliderSm').slick({
+      arrows : false,
+     
+      
+    });
+
+
+
+
+  
+
